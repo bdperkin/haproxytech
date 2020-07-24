afafa3728ff9efcf0c609cc8479754d73b9f44ef
Brandon Perkins <bperkins@redhat.com>
2020-07-24 11:25:47 -0400

    reset

--- rebase.pl
+++ rebase.pl
@@ -96,19 +96,6 @@ foreach my $pkg (@ARGV) {
     my $goipath;
     my $goaltipaths;
     my $version;
-    my $group;
-    my $sources      = "";
-    my $reqs         = "";
-    my $reqsblock    = 0;
-    my $hasbuild     = 0;
-    my $build        = "";
-    my $buildblock   = 0;
-    my $install      = "";
-    my $installblock = 0;
-    my $files        = "";
-    my $filesblock   = 0;
-    my $goprep       = "";
-    my $goprepblock  = 0;
 
     foreach (<SPEC>) {
         if ( $_ =~ m/^%global\s+goipath\s+/ ) {
@@ -123,61 +110,6 @@ foreach my $pkg (@ARGV) {
             my $versionline = $_;
             chomp $versionline;
             ( my $x, $version ) = split( /\s+/, $versionline );
-        }
-        if ( $_ =~ m/^Group:\s+/ ) {
-            $group = $_;
-        }
-        if ( $_ =~ m/^Source\d+:\s+/ ) {
-            $sources = $sources . $_;
-        }
-        if ( $_ =~ m/^%build/ ) {
-            $hasbuild   = 1;
-            $buildblock = 1;
-        }
-        if ( $_ =~ m/^%install/ ) {
-            $buildblock = 0;
-        }
-        if ( $hasbuild eq 1 && $buildblock eq 1 ) {
-            $build = $build . $_;
-        }
-        if ( $_ =~ m/^%install/ ) {
-            $installblock = 1;
-        }
-        if ( $_ =~ m/^%if\s+/ ) {
-            $installblock = 0;
-        }
-        if ( $installblock eq 1 ) {
-            $install = $install . $_;
-        }
-        if ( $_ =~ m/^%files/ ) {
-            $filesblock = 1;
-        }
-        if ( $_ =~ m/^%gopkgfiles/ ) {
-            $filesblock = 0;
-        }
-        if ( $hasbuild eq 1 && $filesblock eq 1 ) {
-            $files = $files . $_;
-        }
-        if ( $goprepblock eq 1 ) {
-            $goprep = $goprep . $_;
-        }
-        if ( $_ =~ m/^%goprep/ ) {
-            $goprepblock = 1;
-        }
-        if ( $_ =~ m/^$/ ) {
-            $goprepblock = 0;
-        }
-        if ( $_ =~ m/^Source\d+:\s+/ ) {
-            $reqsblock = 1;
-        }
-        if ( $_ =~ m/^%description/ || $_ =~ m/^%if\s+/ ) {
-            $reqsblock = 0;
-        }
-        if (   $reqsblock eq 1
-            && $_ !~ m/^BuildRequires:  golang/
-            && $_ !~ /^Source\d+:\s+/ )
-        {
-            $reqs = $reqs . $_;
         }
     }
 
@@ -223,87 +155,23 @@ foreach my $pkg (@ARGV) {
         }
     }
 
-    $buildblock   = 0;
-    $installblock = 0;
-    $filesblock   = 0;
-    $reqsblock    = 0;
-
     foreach (<NEWSPEC>) {
-        if ( $_ =~ m/^BuildRequires:\s+golang/ ) {
-            $reqsblock = 1;
-        }
-        if ( $_ =~ /^$/ && $reqsblock eq 1 && $reqs ) {
-            chomp $reqs;
-            $_         = "$reqs";
-            $reqsblock = 0;
-        }
-        if ( $_ =~ m/^%build/ && $hasbuild eq 1 ) {
-            print SPEC "$build";
-        }
-        if ( $_ =~ m/^%install/ ) {
-            print SPEC "$install";
-        }
-        if ( $_ =~ m/^%files/ && $hasbuild eq 1 ) {
-            print SPEC "$files";
-        }
-        if ( $body eq 1 && $changelog eq 0 ) {
+        if ( $body eq 1 ) {
             if ( $_ =~ m/^Version:\s+/ ) {
                 $_ =~ s/\d+/0/g;
-            }
-            if ( $_ =~ m/^Source\d+:\s+/ ) {
-                $_ = "";
-            }
-            if ( $_ =~ m/^%build/ && $buildblock eq 0 ) {
-                $buildblock = 1;
-            }
-            if ( $_ =~ m/^%install/ && $buildblock eq 1 ) {
-                $buildblock = 0;
-            }
-            if ( $buildblock eq 1 ) {
-                $_ = "";
-            }
-            if ( $_ =~ m/^%install/ && $installblock eq 0 ) {
-                $installblock = 1;
-            }
-            if ( $_ =~ m/^%if\s+/ && $installblock eq 1 ) {
-                $installblock = 0;
-            }
-            if ( $installblock eq 1 ) {
-                $_ = "";
-            }
-            if ( $_ =~ m/^%files/ && $filesblock eq 0 ) {
-                $filesblock = 1;
-            }
-            if ( $_ =~ m/^%gopkgfiles/ && $filesblock eq 1 ) {
-                $filesblock = 0;
-            }
-            if ( $filesblock eq 1 ) {
-                $_ = "";
             }
             print SPEC $_;
         }
         if ( $_ =~ m/^%gometa/ && $goaltipaths ) {
             print SPEC "\n$goaltipaths";
         }
-        if ( $_ =~ m/^Summary:\s+/ && $group ) {
-            print SPEC "\n$group";
-        }
-        if ( $_ =~ m/^URL:\s+/ && $reqsblock eq 0 && $sources ) {
-            print SPEC "$sources";
-        }
-        if ( $_ =~ m/^%goprep/ ) {
-            chomp $goprep;
-            print SPEC "$goprep";
-        }
         if ( $_ =~ m/^# https:\/\/$goipath/ ) {
             $body = 1;
         }
         if ( $_ =~ m/^%changelog/ ) {
-            $changelog = 1;
+            $body = 0;
         }
     }
-
-    $changelog = 0;
 
     seek OLDSPEC, 0, 0;
 
